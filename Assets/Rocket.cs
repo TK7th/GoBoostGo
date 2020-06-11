40b03cad261bd13a372b63dbbbed5fc37deac636
Tyree Fuller <tyreefuller@gmail.com>
2020-06-11 03:17:43 -0400

    Audio added to rocket thrust

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,13 +3,13 @@
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
-    //Transform transform;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-        //transform = GetComponent<Transform>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -18,13 +18,21 @@ public class Rocket : MonoBehaviour
         ProcessInput();
     }
 
-    private void ProcessInput()
+    void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
             print("Thrusting!!!");
             rigidBody.AddRelativeForce(Vector3.up);
 
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Pause();
         }
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
