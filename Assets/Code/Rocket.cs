b05c5f659783e9e09b545858133dc64a3fa2a462
Tyree Fuller <tyreefuller@gmail.com>
2020-06-12 02:29:20 -0400

    Rocket rotates, some members made public to the interface to make adjustments on the fly.

---
+++ Assets/Code/Rocket.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
+        {
+
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Pause();
+        }
+    }
+}
---
+++ Assets/Code/RocketSpin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class RocketSpin : MonoBehaviour
+{
+    float rotateY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(0, rotateY, 0);
+            rotateY += .09f;
+        }
+        else
+        {
+            rotateY = 0;
+        }
+    }
+}
